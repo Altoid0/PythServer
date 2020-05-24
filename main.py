57fd7380015c3215847da84713dd51813223cbef
Tanay Shah <tanay.v.shah@gmail.com>
2020-05-24 15:13:07 -0700

    added login page with new html and css

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'tanay':
+        session['logged_in'] = True
+    else:
+        flash('Wrong password!')
+    return index()
 
 @app.route('/welcome/<name>')
 def welcome(name):
-    return render_template('welcome_page.html', name=name)
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('welcome_page.html', name=name)
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run(debug=True, host='0.0.0.0', port=80)
